cb458c34764d088b6a7171063c93854dedad6e53
Your name <you@example.com>
2021-05-29 14:45:07 +0500

    Fixed time sorting and integer inserts

--- chia_la.py
+++ chia_la.py
@@ -7,6 +7,7 @@ import re
 import sqlite3
 import click
 from tabulate import tabulate
+from datetime import datetime
 
 @click.group()
 def db():
@@ -29,6 +30,9 @@ def get_con():
     return __con
 
 
+def time_convert(timestr: str):
+    return datetime.strptime(timestr, '%a %b %d %H:%M:%S %Y').timestamp()
+
 @click.command()
 def drop_db():
     con = get_con()
@@ -45,18 +49,18 @@ def init_db():
         'id varchar(255) NOT NULL PRIMARY KEY,'
         'tmp text NOT NULL,'
         'dst text, '
-        'size INT DEFAULT 0, '
+        'size INTEGER DEFAULT 0, '
         'buffer_size INT DEFAULT 0, '
-        'threads INT DEFAULT 0, '
-        'buckets INT DEFAULT 0,'
-        'phase_1 DOUBLE DEFAULT 0, '
-        'phase_2 DOUBLE DEFAULT 0, '
-        'phase_3 DOUBLE DEFAULT 0,'
-        'phase_4 DOUBLE DEFAULT 0,'
-        'copy_time DOUBLE,'
-        'complete TINYINT DEFAULT 0,'
-        'start DATETIME,'
-        'end DATETIME'
+        'threads INTEGER DEFAULT 0, '
+        'buckets INTEGER DEFAULT 0,'
+        'phase_1 INTEGER DEFAULT 0, '
+        'phase_2 INTEGER DEFAULT 0, '
+        'phase_3 INTEGER DEFAULT 0,'
+        'phase_4 INTEGER DEFAULT 0,'
+        'copy_time INTEGER,'
+        'complete INTEGER DEFAULT 0,'
+        'start INTEGER,'
+        'end INTEGER'
         ')'
     )
 
@@ -104,20 +108,61 @@ def analyze_logs(logs_dir):
     logs = (i for i in glob(logs_dir + '/*', recursive=False))
 
     patterns = {
-        'id': re.compile('ID: (.*)'),
-        'size': re.compile('Plot size is: (\d\d)'),
-        'buffer_size': re.compile('Buffer size is: (\d*)'),
-        'tmp': re.compile('Starting plotting progress into temporary dirs: ([\w/]+)'),
-        'dst': re.compile('Renamed final file from "(.*?)/plot-k'),
-        'threads': re.compile('Using (\d+) threads'),
-        'buckets': re.compile('Using (\d+) buckets'),
-        'phase_1': re.compile('Time for phase 1 = ([\d.]+)'),
-        'phase_2': re.compile('Time for phase 2 = ([\d.]+)'),
-        'phase_3': re.compile('Time for phase 3 = ([\d.]+)'),
-        'phase_4': re.compile('Time for phase 4 = ([\d.]+)'),
-        'copy_time': re.compile('Copy time = ([\d.]+)'),
-        'start': re.compile('Starting phase 1/4: Forward Propagation into tmp files... (.+)'),
-        'end': re.compile('Copy time .* seconds. CPU \(.*\) (.*)'),
+        'id': {
+            'pattern': re.compile('ID: (.*)'),
+            'modifier': None
+        },
+        'size': {
+            'pattern': re.compile('Plot size is: (\d\d)'),
+            'modifier': None
+        },
+        'buffer_size': {
+            'pattern': re.compile('Buffer size is: (\d*)'),
+            'modifier': int
+        },
+        'tmp': {
+            'pattern': re.compile('Starting plotting progress into temporary dirs: ([\w/]+)'),
+            'modifier': None,
+        },
+        'dst': {
+            'pattern': re.compile('Renamed final file from "(.*?)/plot-k')
+        },
+        'threads': {
+            'pattern': re.compile('Using (\d+) threads'),
+            'modifier': int
+        },
+        'buckets': {
+            'pattern': re.compile('Using (\d+) buckets'),
+            'modifier': int
+        },
+        'phase_1': {
+            'pattern': re.compile('Time for phase 1 = (\d+)'),
+            'modifier': int
+        },
+        'phase_2': {
+            'pattern': re.compile('Time for phase 2 = (\d+)'),
+            'modifier': int
+        },
+        'phase_3': {
+            'pattern': re.compile('Time for phase 3 = (\d+)'),
+            'modifier': int
+        },
+        'phase_4': {
+            'pattern': re.compile('Time for phase 4 = (\d+)'),
+            'modifier': int
+        },
+        'copy_time': {
+            'pattern': re.compile('Copy time = (\d+)'),
+            'modifier': int
+        },
+        'start': {
+            'pattern': re.compile('Starting phase 1/4: Forward Propagation into tmp files... (.+)'),
+            'modifier': time_convert
+        },
+        'end': {
+            'pattern': re.compile('Copy time .* seconds. CPU \(.*\) (.*)'),
+            'modifier': time_convert
+        },
     }
 
     for log in logs:
@@ -126,7 +171,7 @@ def analyze_logs(logs_dir):
         plot_exists = False
         for line in open(log, 'r'):
             for k, p in patterns.items():
-                m = re.match(p, line)
+                m = re.match(p['pattern'], line)
                 if not m:
                     continue
                 val = m.group(1)
@@ -142,7 +187,8 @@ def analyze_logs(logs_dir):
                         plot_exists = True
                         continue
 
-                plot_data[k] = val
+                mdf = p.get('modifier', None)
+                plot_data[k] = val if mdf is None else mdf(val)
         if plot_data:
             insert_plot(plot_data)
 
@@ -153,26 +199,25 @@ def analyze_logs(logs_dir):
     ['id', 'tmp', 'size', 'buffer_size',
      'threads', 'phase_1', 'phase_2', 'phase_3', 'phase_4',
      'copy_time', 'start', 'end']), click.Choice(['DESC', 'ASC'])),
-    default=('start', 'DESC'), help='Sort method <COLUMN> <DIRECTION>')
-
+              default=('start', 'DESC'), help='Sort method <COLUMN> <DIRECTION>')
 @click.option('--bytmp', type=bool, default=False, help='Separate records by tmp dir')
 @click.option('--avg', type=bool, default=False, help='Calculate average values')
 @click.option('--not-completed', type=bool, default=False, help='Include not completed jobs in stat')
 def stat(limit, sort, bytmp, avg, not_completed):
-    fields = ['tmp', 'dst', 'size', 'buffer_size',
-              'threads', 'buckets', 'phase_1', 'phase_2', 'phase_3', 'phase_4', 'copy_time', 'start', 'end', 'complete']
+    fields = ['tmp', 'dst', 'size', 'buffer_size as buffer',
+              'threads', 'buckets', 'phase_1', 'phase_2', 'phase_3', 'phase_4', 'copy_time',
+              'datetime(start, \'unixepoch\', \'localtime\') as start',
+              'datetime(end, \'unixepoch\', \'localtime\') as end', 'complete']
     con = get_con()
     cur = con.cursor()
 
     select = ",".join(fields)
-    headers = fields
     where = 'WHERE '
     if avg:
-        int_fields = [f'AVG({f})' for f in ('phase_1', 'phase_2', 'phase_3', 'phase_4', 'copy_time')]
+        int_fields = [f'CAST(AVG({f}) as integer) as {f}' for f in ('phase_1', 'phase_2', 'phase_3', 'phase_4', 'copy_time')]
         if bytmp:
             int_fields = ['tmp'] + int_fields
         select = ",".join(int_fields)
-        headers = int_fields
     group_by = ''
     if bytmp:
         group_by = 'GROUP BY tmp'
@@ -181,8 +226,9 @@ def stat(limit, sort, bytmp, avg, not_completed):
     where = where if where != 'WHERE ' else ''
     req = f'SELECT {select} FROM plots {where} {group_by} ORDER BY {sort[0]} {sort[1]} LIMIT {limit}'
     data = cur.execute(req).fetchall()
+    headers = [t[0] for t in cur.description]
 
-    print(tabulate(data, headers=headers, showindex="always", tablefmt='pretty', disable_numparse=True))
+    print(tabulate(data, headers=headers, showindex="always", tablefmt='github', disable_numparse=True))
 
 
 db.add_command(init_db)
@@ -192,4 +238,3 @@ db.add_command(stat)
 
 if __name__ == '__main__':
     db()
-
